90767c08a6b814a300717fad84346c0170c0fed8
Himanshu303 <himanshumarathe1611@gmail.com>
2022-07-15 15:24:18 +0530

    Add project files.

---
+++ DllJenkin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Methods;
+namespace DllJenkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            n1 = 5;
+            n2 = 7;
+            MyMaths m = new MyMaths();
+
+            Console.WriteLine(m.Add(n1, n2));
+
+            Console.WriteLine(m.Mul(n1, n2));
+        }
+    }
+}
---
+++ Methods/MyMaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Methods
+{
+    public class MyMaths
+
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
